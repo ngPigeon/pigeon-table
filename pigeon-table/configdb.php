081107771da8614a4ff6b18225172590fccbe7f2
alvinvoonyn <alvinvoonyn@yahoo.com>
2018-02-22 16:18:51 +0800

    Deleted unwanted files

--- pigeon-table/configdb.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define("HOSTNAME","Please insert your MySQL hostname here");
-define("USERNAME","Please insert your MySQL username here");
-define("PASSWORD","Please insert your MySQL password here");
-define("DATABASE", "Please insert the name of the database here");
-
-?>
