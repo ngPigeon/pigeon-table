8661f640e929a9723ff9e497567ea384310d063d
alvinvoonyn <alvinvoonyn@yahoo.com>
2018-02-22 16:21:40 +0800

    Modified includes.php

--- pigeon-table/php/includes.php
+++ pigeon-table/php/includes.php
@@ -3,11 +3,11 @@
 echo "
     <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
     <link href='pigeon-table/css/bootstrap.min.css' rel='stylesheet' />
-	<link href='pigeon-table/css/pigeon-table.css' rel='stylesheet' />
+    <link href='pigeon-table/css/pigeon-table.css' rel='stylesheet' />
     <script src='pigeon-table/js/jquery.min.js'></script>
     <script src='pigeon-table/js/bootstrap.min.js'></script>
-    <script src='pigeon-table/js/angular.min.1.6.4.js'></script>
-    <script src='pigeon-table/js/angular-cookies.min.js'></script>
+    <script src='pigeon-table/js/angular.min.js'></script>
+    <script src='pigeon-table/js/ui-bootstrap-tpls-2.5.0.min.js'></script>
     <script src='pigeon-table/js/pigeon-table.js'></script>";
 
 ?>
